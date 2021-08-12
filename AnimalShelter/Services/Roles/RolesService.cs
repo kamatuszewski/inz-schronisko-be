21c9af69e000c97ef5cc186873d3a69265815520
Radosław Kwiatkowski <s17430@pjwstk.edu.pl>
2021-08-12 17:26:16 +0200

    fixex bug that try to create new entity without checking IsRoleActive flag

--- AnimalShelter/Services/Roles/RolesService.cs
+++ AnimalShelter/Services/Roles/RolesService.cs
@@ -92,14 +92,9 @@ namespace AnimalShelter_WebAPI.Services.Roles
             switch (request.RoleId)
             {
                 case 1: //Volunteer
+
                     var volunteerToSoftDelete = _context.Volunteer.FirstOrDefault(v => v.Id == grantedRoleToRemove.PersonId);
                     volunteerToSoftDelete.IsRoleActive = false;
-
-/*                    var volunteerToSoftDelete = new Volunteer()
-                    {
-                        Id = grantedRoleToRemove.PersonId,
-                        IsRoleActive = false
-                    };*/
 
                     _context.Volunteer.Update(volunteerToSoftDelete);
                     _context.SaveChanges();
@@ -130,23 +125,46 @@ namespace AnimalShelter_WebAPI.Services.Roles
             switch (request.RoleId)
             {
                 case 1: //Volunteer
-                    var newVolunteer = _mapper.Map<Volunteer>(request);
-                    newVolunteer.Id = grantedRoleEntity.PersonId;
-                    _context.Volunteer.Add(newVolunteer);
-                    _context.SaveChanges();
+                    var volunteerExists = _context.Volunteer.FirstOrDefault(gr => gr.Id == grantedRoleEntity.PersonId);
+                    if (volunteerExists is not null)
+                    {
+                        volunteerExists.IsRoleActive = true;
+                        volunteerExists.JoiningDate = (DateTime)request.JoiningDate;
+                        volunteerExists.Attendance = request.Attendance;
+                        _context.Volunteer.Update(volunteerExists);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        var newVolunteer = _mapper.Map<Volunteer>(request);
+                        newVolunteer.Id = grantedRoleEntity.PersonId;
+                        _context.Volunteer.Add(newVolunteer);
+                        _context.SaveChanges();
+                    }
                     break;
+
                 case 5: //Vet
 
-                    
                     var empExists = _context.Employee.FirstOrDefault(gr => gr.Id == grantedRoleEntity.PersonId);
                     if (empExists is not null)
                     {
-                        var newVet = _mapper.Map<Vet>(request);
-                        newVet.Id = grantedRoleEntity.PersonId;
-                        _context.Vet.Add(newVet);
-                        _context.SaveChanges();
+                        var vetExists = _context.Vet.FirstOrDefault(gr => gr.Id == grantedRoleEntity.PersonId);
+                        if (vetExists is not null)
+                        {
+                            vetExists.IsRoleActive = true;
+                            vetExists.PWZNumber = request.PWZNumber;
+                            _context.Vet.Update(vetExists);
+                            _context.SaveChanges();
 
-                    } else //we are creating Vet so Emp Entity IsRoleActive should be set to false 
+                        } else
+                        {
+                            var newVet = _mapper.Map<Vet>(request);
+                            newVet.Id = grantedRoleEntity.PersonId;
+                            _context.Vet.Add(newVet);
+                            _context.SaveChanges();
+                        }
+
+                    } else  
                     {
                         var newEmp = _mapper.Map<Employee>(request);
                         newEmp.Id = grantedRoleEntity.PersonId;
@@ -156,18 +174,31 @@ namespace AnimalShelter_WebAPI.Services.Roles
                         var newVet = _mapper.Map<Vet>(request);
                         newVet.Id = grantedRoleEntity.PersonId;
                         _context.Vet.Add(newVet);
-
                         _context.SaveChanges();
                     }
                     break;
+
                 case 2: //Emp
-                    var newEmpl = _mapper.Map<Employee>(request);
-                    newEmpl.Id = grantedRoleEntity.PersonId;
-                    _context.Employee.Add(newEmpl);
 
-                    _context.SaveChanges();
+                    var empExists2 = _context.Employee.FirstOrDefault(gr => gr.Id == grantedRoleEntity.PersonId);
+                    if (empExists2 is not null)
+                    {
+                        empExists2.IsRoleActive = true;
+                        empExists2.HireDate = (DateTime)request.HireDate;
+                        empExists2.Salary = request.Salary;
+                        empExists2.QuitDate = null;
+                        _context.Employee.Update(empExists2);
+                        _context.SaveChanges();
+
+                    } else
+                    {
+                        var newEmpl = _mapper.Map<Employee>(request);
+                        newEmpl.Id = grantedRoleEntity.PersonId;
+                        _context.Employee.Add(newEmpl);
+                        _context.SaveChanges();
+                    }
+
                     break;
-
                 default:
                     break;
             }
